dd36a5e50f399ed1472ba3d4bb1c14a1017f862c
Karine Bialobroda <karine.bialobroda@gmail.com>
2016-05-04 19:01:30 +0200

    small fix

--- source/javascripts/navbar-services.js
+++ source/javascripts/navbar-services.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
 
-  var anchor_offset = $('#banner-2').offset().top;
-
   $(window).on('scroll', function() {
     if ( $(window).scrollTop() > 0) {
       $('#navbar').css('top', '0');
