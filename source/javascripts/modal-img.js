dec658eaa6c98e3ff4f770bddf7e587fcd79ee72
Karine Bialobroda <karine.bialobroda@gmail.com>
2017-01-08 18:55:13 +0100

    add press

---
+++ source/javascripts/modal-img.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function() {
+	$(document).on("click", ".modal-link", function() {
+		var imageId = $(this).parents(".picture-card").attr("id");
+		var imageUrl = document.getElementById(imageId).style.backgroundImage;
+		var imageSrc = "images/gallery/gallery" + imageId + ".jpg"
+		$("#modal-img").closest
+		$("#image-src").attr("src", imageSrc);
+	});
+});
